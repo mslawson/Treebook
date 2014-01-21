99499435288c0277961cd2618920df7a951957f9
Matt Slawson <mdslawson@gmail.com>
2014-01-21 16:11:40 -0500

    Added testing for User Friendships

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
 	user = User.new
 	assert !user.save
@@ -28,6 +31,19 @@ class UserTest < ActiveSupport::TestCase
     assert user.valid?
 
   end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:matt).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do 
+    users(:matt).friends << users(:mike)
+    users(:matt).friends.reload
+    assert users(:matt).friends.include?(users(:mike))
+  end
+
 
 
  #  test "a user should have a profile name without spaces" do
