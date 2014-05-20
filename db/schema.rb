08850975173ab2bce824f89d77103b43ec7e2481
Matt Slawson <mdslawson@gmail.com>
2014-05-20 16:47:15 -0400

    added documents

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140207215856) do
+ActiveRecord::Schema.define(:version => 20140509183406) do
+
+  create_table "documents", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+    t.string   "add_attachment_file_name"
+    t.string   "add_attachment_content_type"
+    t.integer  "add_attachment_file_size"
+    t.datetime "add_attachment_updated_at"
+  end
+
+  add_index "documents", ["user_id"], :name => "index_documents_on_user_id"
 
   create_table "statuses", :force => true do |t|
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.integer  "document_id"
   end
 
   add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
