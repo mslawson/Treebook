b5c12a44fbdebb1f68a10f272b2612dac015a4b0
Matt Slawson <mdslawson@gmail.com>
2014-05-21 19:58:30 -0400

    resized images. saving properly. displaying properly on index, but still an error on the edit user page in regards to display

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,13 +47,23 @@ class User < ActiveRecord::Base
   has_many :accepted_friends, through: :accepted_user_friendships, source: :friend   
 
   has_attached_file :avatar, styles: {
-    large: "800x800>", medium: "300x200>", small: "110x110!", thumb: "80x80!"
+    large: "800x800>", medium: "300x200>", small: "260x180!", thumb: "80x80!"
   }
 
   do_not_validate_attachment_file_type :avatar
 
   def full_name
   	first_name + " " + last_name
+  end
+
+  def self.get_gravatars
+    all.each do |user|
+      if !user.avatar?
+        user.avatar = URI.parse(user.gravatar_url)
+        user.save
+        print "."
+      end
+    end
   end
 
   def gravatar_url
